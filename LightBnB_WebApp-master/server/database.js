799eb0fa722b8a2ce75d9fabd91cc26f69ef5b13
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-07 21:45:29 +0000

    queries for getting all reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -49,7 +49,7 @@ const getUserWithId = function(id) {
   return pool.query(`
     SELECT * 
     FROM users
-    WHERE id = $1
+    WHERE id = $1;
   `, [id])
   .then(res => res.rows[0])
   .catch(err => {
@@ -86,7 +86,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  
+  return pool.query(`
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then(res => res.rows[0])
+  .catch(res => console.log("no reservation"))
+  
+  //return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
